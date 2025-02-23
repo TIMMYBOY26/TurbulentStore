0427bcac5e271dfc8e2cc9dcdbd58e081eea4c32
Timmyboy26 <ghui3j@gmai.com>
2025-02-24 00:50:10 +0800

    change vite config to map host

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5174 }
-})
+  server: {
+    host: "0.0.0.0", // Allow access from any IP address
+    port: 5174, // Specify the port
+  },
+});
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 }
-
-})
+  server: {
+    host: "0.0.0.0", // Allow access from any IP address
+    port: 5173, // Specify the port
+  },
+});
