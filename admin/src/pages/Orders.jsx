0947f14c6a1d3ab9027296ea282f152e41b45213
TIMMYBOY26 <71860516+TIMMYBOY26@users.noreply.github.com>
2025-05-14 15:57:20 +0800

    remove comer in in order listing

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -232,14 +232,14 @@ const Orders = ({ token }) => {
                 {order.address.firstName + " " + order.address.lastName}
               </p>
               <div>
-                <p>{order.address.street + ","}</p>
+                <p>{order.address.street + ""}</p>
                 <p>
                   {order.address.city +
-                    "," +
+                    "" +
                     order.address.state +
-                    "," +
+                    "" +
                     order.address.country +
-                    "," +
+                    "" +
                     order.address.zipcode}
                 </p>
               </div>
