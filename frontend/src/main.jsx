cdb28c761dbbd462119f4592c11eb5146db3f3cf
TIMMYBOY26 <71860516+TIMMYBOY26@users.noreply.github.com>
2026-01-26 15:16:08 +0800

    secret art

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,10 +5,27 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import ShopContextProvider from "./context/ShopContext.jsx";
 
+// --- START EASTER EGG ---
+const secretArt = `
+  ______   __  __   ____    ____   _    _   _        ______   _   _   _______ 
+ |__   __| |  | |  |  _ \\  |  _ \\ | |  | | | |      |  ____| | \\ | | |__   __|
+    | |    |  | |  | |_) | | |_) || |  | | | |      | |__    |  \\| |    | |   
+    | |    |  | |  |  _ <  |  _ < | |  | | | |      |  __|   | . \` |    | |   
+    | |    |__| |  | |_) | | |_) || |__| | | |____  | |____  | |\\  |    | |   
+    |_|    \\____/  |____/  |____/  \\____/  |______| |______| |_| \\_|    |_|   
+                                                                              
+`;
+
+console.log(
+  `%c${secretArt}`,
+  "font-family: monospace; color: #00FF00; background: #000; font-weight: bold; padding: 10px;",
+);
+// --- END EASTER EGG ---
+
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <ShopContextProvider>
       <App />
     </ShopContextProvider>
-  </BrowserRouter>
+  </BrowserRouter>,
 );
