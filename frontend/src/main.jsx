ed67e8c7a758817a77a0b7a3bee8b69908ff36d6
TIMMYBOY26 <71860516+TIMMYBOY26@users.noreply.github.com>
2026-02-09 12:31:27 +0800

    try app implement

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,11 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import ShopContextProvider from "./context/ShopContext.jsx";
+// --- ADD THIS IMPORT ---
+import { registerSW } from "virtual:pwa-register";
+
+// Automatically updates the app when you push new changes to Vercel
+registerSW({ immediate: true });
 
 // --- START LONG PORTRAIT EASTER EGG ---
 const longArt = `
