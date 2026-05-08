ec4827703d379282478480241fdd282558cfd544
TIMMYBOY26 <71860516+TIMMYBOY26@users.noreply.github.com>
2026-05-08 14:57:27 +0800

    update font size

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -183,7 +183,7 @@ const PlaceOrder = () => {
                   ) : (
                     <div className="text-center py-2">
                       <svg className="w-6 h-6 text-gray-400 mx-auto mb-2" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 16v1a2 2 0 002 2h12a2 2 0 002-2v-1m-4-8l-4-4m0 0L8 8m4-4v12" /></svg>
-                      <p className="text-[10px] uppercase font-bold tracking-widest text-gray-400">Upload Receipt (Optional)</p>
+                      <p className="text-[15px] uppercase font-bold tracking-widest text-gray-400">Upload Receipt</p>
                     </div>
                   )}
                   <input type="file" className="hidden" accept="image/*" onChange={(e) => setReceiptImage(e.target.files[0])} />
